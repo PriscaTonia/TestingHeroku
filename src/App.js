6c512a385bdd97ae38627e1213fff8a434d07982
PriscaTonia <priscaebube@gmail.com>
2020-11-02 16:24:22 +0100

    commiting the first changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component{
+
+  state ={
+    name: "Prisca",
+    age: 30
+  }
+  handleClick = (e) =>{
+    //console.log(e.target);
+    console.log(this.state.age)
+    this.setState({
+      name: "Florence",
+      age: 18
+    })
+  }
+  handleMouseOver =(e) =>{
+    console.log(e.target, e.pageX);
+  }
+  render(){
+    return(
+      <div className='myApp'>
+        <h1>Hi, Welcome!</h1>
+        <p> My name is: {this.state.name} and I am {this.state.age}</p>
+        <button onClick = {this.handleClick}>Click me</button>
+        <button onMouseOver={this.handleMouseOver}>Hover me</button>
+      </div>
+    )
+  }
 }
+
 
 export default App;
